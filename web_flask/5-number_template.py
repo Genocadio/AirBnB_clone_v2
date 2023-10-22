90a915e51a426a36979758037e83661141e0a9e4
geno <yvesgeno@outlook.com>
2023-10-22 22:32:42 +0200

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """a script that starts a Flask web application: """
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -30,6 +30,18 @@ def c(text):
 def py(text='is cool'):
     """  display python """
     return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """  display n is a number """
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """  display n is a number """
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
